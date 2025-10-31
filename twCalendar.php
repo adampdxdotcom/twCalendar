5cafc6db0be4e8ed29c066c1429ebec5587be1e1
adampdxdotcom <129908610+adampdxdotcom@users.noreply.github.com>
2025-10-31 07:46:27 -0700

    Update twCalendar.php

--- twCalendar.php
+++ twCalendar.php
@@ -19,3 +19,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 require_once plugin_dir_path( __FILE__ ) . 'includes/shortcode-router.php';
+require_once plugin_dir_path( __FILE__ ) . 'includes/cal-functions.php';
+require_once plugin_dir_path( __FILE__ ) . 'includes/play_date_list.php';
+require_once plugin_dir_path( __FILE__ ) . 'includes/view_grid_list.php';
+require_once plugin_dir_path( __FILE__ ) . 'includes/view_scroll.php';
+require_once plugin_dir_path( __FILE__ ) . 'includes/view_scroll_featured.php';
+
+
+
