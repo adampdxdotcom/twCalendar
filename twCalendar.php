afa11ba380ac6e203545aee055d24263af369317
adampdxdotcom <129908610+adampdxdotcom@users.noreply.github.com>
2025-10-31 07:10:40 -0700

    Create twCalendar.php

---
+++ twCalendar.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name:       TW Calendar
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       A custom calendar plugin built for the Theatre West website
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Adam Michaels
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       tw-calendar
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Main plugin code will go here...
